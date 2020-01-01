5b9425176bca89f0c9ad1546de4baa95e8f6c1a5
Andrii <andrii.symonenko.dev@gmail.com>
2020-01-01 22:55:46 +0200

    add timer and random value

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 let $start = document.querySelector("#start");
 let $game = document.querySelector("#game");
+let $time = document.querySelector("#time");
 let score = 0;
+
+let isGameStarted = false;
 $start.addEventListener("click", startGame);
 $game.addEventListener("click", boxClick);
 
 function startGame() {
+  isGameStarted = true;
   $game.style.backgroundColor = "#fff";
   $start.classList.add("hide");
+
+  let interval = setInterval(function() {
+    // таймер
+    let time = parseFloat($time.textContent); // Позволяет задавать или получать текстовое содержимое элемента и его потомков.
+
+    if (time <= 0) {
+      clearInterval(interval);
+      endGame();
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1);
+    }
+  }, 100);
+
   renderBox();
 }
 
+function endGame() {
+  isGameStarted = false;
+}
+
 function boxClick(event) {
+  if (!isGameStarted) {
+    return; // выход из функцмм по окончанию времени игры
+  }
   if (event.target.dataset.box) {
     // проверяет наличие box по клику
     score++;
@@ -20,13 +44,21 @@ function boxClick(event) {
 
 function renderBox() {
   let box = document.createElement("div");
+  let boxSize = getRandom(30, 100);
+  let gameSize = $game.getBoundingClientRect(); //объект с размерами
+  let maxTop = gameSize.height - boxSize;
+  let maxLeft = gameSize.width - boxSize;
   $game.innerHTML = "";
-  box.style.height = box.style.width = "50px";
+  box.style.height = box.style.width = boxSize + "px";
   box.style.position = "absolute";
   box.style.backgroundColor = "#000";
-  box.style.top = "50px";
-  box.style.left = "70px";
+  box.style.top = getRandom(0, maxTop) + "px";
+  box.style.left = getRandom(0, maxLeft) + "px";
   box.style.cursor = "pointer";
   box.setAttribute("data-box", true); // атрибут для поиска
   $game.insertAdjacentElement("afterbegin", box); // добавляет переданный элемент в DOM-дерево относительно элемента, вызвавшего метод.
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
